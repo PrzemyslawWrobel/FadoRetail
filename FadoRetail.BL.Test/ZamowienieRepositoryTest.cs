30d815c339d4e7a18881e37c65ab90ec11aaff68
PW <przemek.wrobel@poczta.fm>
2020-03-04 09:38:07 +0100

    Klasa zamówienieRepository + test

---
+++ FadoRetail.BL.Test/ZamowienieRepositoryTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FadoRetail.BL.Test
+{
+    [TestClass]
+    public class ZamowienieRepositoryTest
+    {
+        [TestMethod]
+        public void PobierzZamowienieTest()
+        {
+            // Arrange (przygotuj test)
+
+            var zamowienieRepository = new ZamowienieRepository();
+            var oczekiwana = new Zamowienie(10)
+            {
+                DataZamowienia = new DateTimeOffset(2020, 3, 4, 11, 00, 00, new TimeSpan(7, 0, 0))
+            };
+            //Avt (działaj)
+            var aktualne = zamowienieRepository.Pobierz(10);
+
+
+            //Asert (potwierdź)
+            Assert.AreEqual(oczekiwana.ZamowienieId, aktualne.ZamowienieId);
+            Assert.AreEqual(oczekiwana.DataZamowienia, aktualne.DataZamowienia);
+        }
+    }
+}
---
+++ FadoRetail.BL/ZamowienieRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FadoRetail.BL
+{
+    public class ZamowienieRepository
+    {
+
+        /// <summary>
+        /// Pobiera jedno zamówienie
+        /// </summary>
+        /// <param name="zamowienieId"></param>
+        /// <returns></returns>
+        public Zamowienie Pobierz(int zamowienieId)
+        {
+            Zamowienie zamowienie = new Zamowienie(zamowienieId);
+            // kod który pobiera określone zamówienie
+
+            //Tymczasowo zakodowane
+            if(zamowienieId == 10)
+            {
+                zamowienie.DataZamowienia = new DateTimeOffset(2020, 3, 4, 11, 00, 00, new TimeSpan(7, 0, 0));
+
+            }
+
+            return zamowienie;
+        }
+
+        /// <summary>
+        /// Pobiera wszystkie zamówienia
+        /// </summary>
+        /// <returns></returns>
+        public List<Zamowienie> PobierzWszystkich()
+        {
+            // kod który pobiera wszystkie zamówienia
+            return new List<Zamowienie>();
+        }
+
+        /// <summary>
+        /// Metoda zapisuje zmówienie
+        /// </summary>
+        /// <returns></returns>
+        public bool Zapisz()
+        {
+            // zapisuje zdefiniowane zamówienie
+            return true;
+        }
+
+
+    }
+}
